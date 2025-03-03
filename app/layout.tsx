65f63287bb55349b5d07dde01168db5b7c27e883
Your Name <eurayssamagalhaes@gmail.com>
2025-03-03 18:33:12 -0300

    Aplicando uma renderização dinâmica

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" data-lt-installed="true">
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
