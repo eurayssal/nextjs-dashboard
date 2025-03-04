2f062a48ae5b3bdf95eb80575190776ce0475022
Your Name <eurayssamagalhaes@gmail.com>
2025-03-03 22:34:15 -0300

    Aplicação da Table | Search | Paginação

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,5 @@
 import SideNav from "../ui/dashboard/sidenav";
-/*
-Um benefício de usar layouts no Next.js é que, na navegação, apenas os 
-componentes da página são atualizados, enquanto o layout não é renderizado novamente.
-Isso é chamado de renderização parcial, que preserva o estado React do lado do cliente 
-no layout ao fazer a transição entre as páginas.
-*/
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
